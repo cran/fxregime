3c42690a72212b1fdb4d0191bf077065cad628a9
Achim Zeileis <Achim.Zeileis@R-project.org>
2008-08-18 00:00:00 +0000

    version 0.2-0

--- R/fxlm.R
+++ R/fxlm.R
@@ -18,15 +18,46 @@ coef.fxlm <- function(object, ...) {
   return(rval)
 }
 
+## estfun.fxlm <- function(x, ...) {
+##   res <- residuals(x)
+##   ef <- NextMethod(x)
+##   sigma2 <- mean(res^2)
+##   rval <- cbind(ef, (res^2 - sigma2))
+##   colnames(rval) <- c(colnames(ef), "(Variance)")
+##   if(!inherits(rval, "zoo"))
+##     rval <- zoo(rval, index(x))
+##   return(rval)
+## }
+## 
+## bread.fxlm <- function(x, ...) {
+##   br <- NextMethod(x)
+##   sigma2 <- coef(x)["(Variance)"]
+##   br <- rbind(cbind(br, "(Variance)" = 0), "(Variance)" = 0)
+##   br[nrow(br), ncol(br)] <- 1
+##   br
+## }
+
 estfun.fxlm <- function(x, ...) {
   res <- residuals(x)
   ef <- NextMethod(x)
   sigma2 <- mean(res^2)
-  rval <- cbind(ef, res^2 - sigma2)
+  rval <- cbind(ef/sigma2, (res^2 - sigma2)/(2 * sigma2^2))
   colnames(rval) <- c(colnames(ef), "(Variance)")
   if(!inherits(rval, "zoo"))
     rval <- zoo(rval, index(x))
   return(rval)
+}
+
+bread.fxlm <- function(x, ...) {
+  br <- NextMethod(x)
+  sigma2 <- coef(x)["(Variance)"]
+  ef <- estfun(x)
+  b1 <- solve(br)/sigma2
+  b2 <- colMeans(ef[,-ncol(ef)]/sigma2)
+  b3 <- 0.5/(sigma2^2)
+    
+  br <- rbind(cbind(b1, "(Variance)" = b2), "(Variance)" = c(b2, b3))
+  solve(br)
 }
 
 ## index/time extraction
